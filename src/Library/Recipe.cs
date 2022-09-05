303dc767a6c4bceb806459a5eeb64a4d8e6ffb20
GeorgeA18 <jorgelars18@gmail.com>
2022-09-05 08:38:46 -0300

    confirmation

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -33,6 +33,26 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+            Console.WriteLine($"El costo total de la producución es: {GetProductionCost()}");
+        }
+
+        /*
+        He decidio que esta clase deberia de tener la responsabilidad de obtener el toal del costo de producción ya que esta es la cual contiene toda la información necesaria para realizar dicha tarea. he llegado a esta conclusión siguiendo el principio EXPERT.
+        */
+        private double GetProductionCost()
+        {   
+            double totalSupplies = 0;
+            double totalEquipment = 0;
+            double Total = 0;
+            foreach(Step step in this.steps)
+            {   
+                totalSupplies = totalSupplies + (step.Input.UnitCost*step.Quantity);
+                totalEquipment =  totalEquipment + ((step.Equipment.HourlyCost/60)*step.Time);
+                Total = totalSupplies+totalEquipment;
+            }
+
+            return Total;
         }
     }
+
 }
